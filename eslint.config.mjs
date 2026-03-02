f6f263c74c6731124f416ba8d6dc08841e0ab3cb
Alexandre Phiev <alexandre.phiev@gmail.com>
2026-03-02 18:06:34 +0100

    feat: upgrade and clean eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,45 +1,12 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import js from '@eslint/js'
-import typescriptEslint from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
-import prettier from 'eslint-plugin-prettier'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-})
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
-const config = [
-  js.configs.recommended,
-  ...compat.extends(
-    '@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ),
-  {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-      prettier,
-    },
-    languageOptions: { 
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-irregular-whitespace': 'off',
-      curly: 'error',
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/explicit-function-return-type': 'warn',
-    },
-  },
-]
-
-export default config
+export default defineConfig(
+  { ignores: ['dist/**', 'node_modules/**'] },
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+)
